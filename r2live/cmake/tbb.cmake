68c88020ffe6685f3acc9e9d6eff8e8db1d67ebf
liming-jing <jlm824307272@163.com>
2023-03-20 11:15:25 +0800

    add tbb.cmake

---
+++ r2live/cmake/tbb.cmake
@@ -0,0 +1,8 @@
+# /usr/local/lib/libtbb.so
+# /usr/local/lib/libtbbmalloc.so
+# /usr/local/lib/libtbbmalloc_proxy.so
+
+list(APPEND ALL_TARGET_LIBRARIES 
+    /usr/local/lib/libtbb.so
+    /usr/local/lib/libtbbmalloc.so
+    /usr/local/lib/libtbbmalloc_proxy.so)
